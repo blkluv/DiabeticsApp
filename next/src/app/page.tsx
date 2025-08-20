824447789eca8c2ed6ff54e5809c983ec94abf71
Katie Alley <112027177+katiealleyvt@users.noreply.github.com>
2025-08-20 18:55:59 -0400

    Added menu buttons

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -7,11 +7,14 @@ import { Meal, Total } from "./models/meals";
 import MealTable from "./components/Meals/MealTable";
 import TotalComponent from "./components/Total";
 import { User } from "./models/user";
+import { FaBowlFood , FaBurger} from "react-icons/fa6";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
 
   const [meals, setMeals] = useState<Meal[]>([{name:'',total_carbs:0,food_items: []}]);
   const [total, setTotal] = useState<Total>({carbs: 0, insulin: 0})
+  // TODO: Change userdata to context
   const [user, setUser] = useState<User>({carb_ratio: 7})
 
  // Get meals and user data, applying carbratio to meals
@@ -52,7 +55,7 @@ export default function Home() {
   }
   //
   return (
-    <div className="font-sans container my-6 mx-30">
+    <div className="font-sans container my-6 mx-30 ">
         <PageTabs id={1}/>
         <div className="m-32">
           <div className="mb-10">
@@ -61,10 +64,12 @@ export default function Home() {
             Insulin Carb Ratio: {user?.carb_ratio}
             <MealTable data={meals} addTotal={addTotal}/>
           </div>
-          <div id="actions">
-          </div>
+          
         </div>
-        
+        <div id="actions-menu">
+          <Button className="menu-button" id="createMealBtn"><FaBurger /></Button>
+            <Button className="menu-button" id="createMealBtn"><FaBowlFood /></Button>
+          </div>
         <TotalComponent total={total} clearTotal={clearTotal}/>
     </div>
   );
